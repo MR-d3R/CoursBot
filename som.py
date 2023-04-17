d8d58623b41de09c8eb8f7e3f87bc08049705995
Alexeev Eduard <47676479+MR-d3R@users.noreply.github.com>
2023-04-17 23:55:54 +0300

    example database and bot files added

---
+++ som.py
@@ -0,0 +1,20 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = client["Courses"]
+mycol = mydb["Course"]
+
+mydict = {"name": "Курс Python", "link": "https://www.python.org/", "tags": ["python"]}
+
+# print(mydb.list_collection_names())
+x = mycol.insert_one(mydict)
+# db.inventory.insert_one(
+#     {
+#         "item": "canvas",
+#         "qty": 100,
+#         "tags": ["cotton"],
+#         "size": {"h": 28, "w": 35.5, "uom": "cm"},
+#     }
+# )
+# cursor = db.inventory.find({"item": "canvas"})
