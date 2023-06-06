fbd0474423dd92513f35a023a67c1290e74fde48
Ramis <gcfhcjh@gmail.com>
2023-06-07 04:43:32 +0500

    improved the interface

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import asyncio
 import logging
 import some, os
+import data_emoji
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.types import FSInputFile
@@ -21,8 +23,14 @@ dp = Dispatcher()
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
+    photo = FSInputFile(f"hello.jpg")
+    await bot.send_photo(
+        message.from_user.id,
+        photo,
+    )
     await message.answer(
-        "Привет! Напиши, что хочешь сгенерировать, и получи картинку!\n\nНецензурные слова и слова 18+ бот не принимает."
+        f"{data_emoji.emojDiamont}Привет!{data_emoji.emojDiamont}\n Напиши, что хочешь сгенерировать, и получи картинку!"
+        f"\n\nНецензурные слова и слова 18+ бот не принимает."
     )
 
 
@@ -30,13 +38,14 @@ async def cmd_start(message: types.Message):
 async def send_answer(message: types.Message):
     try:
         user_input = message.text
-        await bot.send_message(message.from_user.id, f"Ожидайте...")
+        await bot.send_message(message.from_user.id, f"{data_emoji.emojiZont}Ожидайте...{data_emoji.emojiZont}")
         file_name = some.image_generator(user_input)
         photo = FSInputFile(f"{file_name}")
         await bot.send_photo(
             message.from_user.id,
             photo,
-            caption=f"Результат изображения по запросу: {user_input}",
+            caption=f"{data_emoji.emojMolnia}Результат изображения по запросу: {user_input} {data_emoji.emojMolnia}",
+
         )
 
         os.remove(file_name)
---
+++ data_emoji.py
@@ -0,0 +1,9 @@
+import emoji
+
+
+emojDiamont = emoji.emojize(":diamond_with_a_dot:")
+emojMolnia = emoji.emojize(":high_voltage:")
+emoji100 = emoji.emojize(":hundred_points:")
+emojiBeg = emoji.emojize(":person_running:")
+emojiZont = emoji.emojize(":umbrella:")
+emojiMONEY = emoji.emojize(":credit_card:")
